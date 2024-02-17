98645898f0c04effb7269650b401d4bfa11866d6
trainb0y <trainb0ydev@gmail.com>
2024-02-17 15:51:19 -0700

    renaming things + credits/end scene

---
+++ Scripts/creditscroll.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var label: RichTextLabel = $RichTextLabel
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+const speed = 0.05
+var cooldown: float = 0 
+
+func _ready():
+	label.visible_characters = 0
+	audio.play()
+
+func _process(delta):
+	cooldown -= delta
+	if cooldown < 0:
+		label.visible_characters += 1
+		cooldown += speed
+	
+	if label.visible_ratio >= 1:
+		audio.stop()
+
+func _on_audio_stream_player_2d_finished():
+	audio.play()
