9c6f686a8f603128a778ac741f6009b8a1213c67
trainb0y <trainb0ydev@gmail.com>
2024-02-17 13:50:39 -0700

    level end flag sprite

--- Scripts/levelend.gd
+++ Scripts/levelend.gd
@@ -2,7 +2,9 @@ extends Area2D
 
 @export_file(".tscn") var next_level: String
 @export_file(".tscn") var current_level: String
+
 @onready var t: Transition = Util.get_transition()
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready():
 	owner.connect("ready", _on_scene_ready)
@@ -13,6 +15,7 @@ func _on_scene_ready():
 func _on_body_entered(body):
 	if body is Player:
 		t.into_black()
+		audio.play()
 		await get_tree().create_timer(0.4).timeout
 		get_tree().change_scene_to_file(next_level)
 		
